56bd47ad9f63caec5562484d2b6415522f4c5219
22bipp22 <tammibippert@gmail.com>
2020-10-05 16:23:09 -0500

    connected pandas to SQL and wrote to table.

--- places.sql
+++ places.sql
@@ -1,8 +1,10 @@
-CREATE TABLE places (
-	state VARCHAR(30) NOT NULL,
-	avg_income int,
-	avg_home_cost int,
-	avg_property_tax int,
-	avg_auto_ins_premium int,
-	unemployment_rate int
-	)
+-- CREATE TABLE places (
+-- 	state VARCHAR(30) NOT NULL,
+-- 	avg_income int,
+--  	avg_home_cost int,
+--  	avg_property_tax int,
+--  	avg_auto_ins_premium int,
+--  	unemployment_rate int
+--  	);
+
+SELECT * from places;
